48f417f0eb56aca059970c8bc13b3c17b41303d0
Sam <44429208+MegaSamax@users.noreply.github.com>
2020-12-09 13:12:17 +0000

    Added Navigation

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 import {
   Button,
   StyleSheet,
@@ -13,26 +15,28 @@ import { CurrentTask } from "./components/CurrentTask.js";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingBottom: 10,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-      }}
-    >
-      <View style={{ flex: 10 }}>
-        <Header />
-      </View>
+    <NavigationContainer>
+      <View
+        style={{
+          flex: 1,
+          paddingLeft: 10,
+          paddingRight: 10,
+          paddingBottom: 10,
+          paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        }}
+      >
+        <View style={{ flex: 10 }}>
+          <Header />
+        </View>
 
-      <View style={{ flex: 45 }}>
-        <RandomTask />
-      </View>
+        <View style={{ flex: 45 }}>
+          <RandomTask />
+        </View>
 
-      <View style={{ flex: 45 }}>
-        <CurrentTask />
+        <View style={{ flex: 45 }}>
+          <CurrentTask />
+        </View>
       </View>
-    </View>
+    </NavigationContainer>
   );
 }
